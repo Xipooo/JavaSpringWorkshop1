1063d07d529bddf94ff028a38f89874e2bb8daf5
Steve <Steve@DESKTOP-BL6JD8J>
2018-04-10 10:18:08 -0700

    Added project

---
+++ JavaSpringWorkshop1/src/main/java/com/wozu/JavaSpringWorkshop1/JavaSpringWorkshop1Application.java
@@ -0,0 +1,12 @@
+package com.wozu.JavaSpringWorkshop1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWorkshop1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWorkshop1Application.class, args);
+	}
+}
---
+++ JavaSpringWorkshop1/src/test/java/com/wozu/JavaSpringWorkshop1/JavaSpringWorkshop1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wozu.JavaSpringWorkshop1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaSpringWorkshop1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
